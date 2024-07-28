011d240b7783036084c15e2dac65575e5256baa5
elmurodvokhidov <elmurodvkh@gmail.com>
2024-07-28 21:43:02 +0500

    authentication - clerk

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,14 +1,20 @@
 import { Editor } from "@/components/editor/Editor"
 import Header from "@/components/Header"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
 
 const Document = () => {
     return (
         <div>
             <Header>
                 <div className="flex w-fit items-center justify-center gap-2">
-                    <p className="document-title">This is a fake document title</p>
+                    <p className="document-title">Share</p>
                 </div>
-                <p className="text-white">Test</p>
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </Header>
             <Editor />
         </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css"
 import { cn } from "@/lib/utils"
 import React from "react"
 import { Metadata } from "next"
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -17,15 +19,25 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: {
+          colorPrimary: '#3371ff',
+          fontSize: '16px',
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
